2d900e71c1c244fc52c618a604c10532051c7cdd
kacha930 <robiwere20@gmail.com>
2024-07-22 05:45:53 +0300

    success

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+const findMatching = (arr, driver) => {
+    return arr.filter((item) => item.toLowerCase() === driver.toLowerCase());
+  };
+  
+  const fuzzyMatch = (arr, searchString) => {
+    return arr.filter((item) =>
+      item.toLowerCase().startsWith(searchString.toLowerCase())
+    );
+  };
+  //return emoty if no match found
+  const matchName = (arr, name) => {
+    return arr.filter((item) => item && item.name === name);
+  };
